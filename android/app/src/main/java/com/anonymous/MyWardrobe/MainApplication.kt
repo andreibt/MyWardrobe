974391e5c9335d97670d657e0b8171dde161b2c9
Andrei Bogdan <31503124+andreibt@users.noreply.github.com>
2026-07-05 00:50:17 +0300

    Update Android build

--- android/app/src/main/java/com/anonymous/MyWardrobe/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/MyWardrobe/MainApplication.kt
@@ -2,7 +2,6 @@ package com.anonymous.mywardrobe
 
 import android.app.Application
 import android.content.res.Configuration
-import com.anonymous.mywardrobe.BuildConfig
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
